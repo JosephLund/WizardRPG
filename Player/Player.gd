3a6c03dc58c473f8a0b6e9422d3aa71159b87da1
Joseph Lund <skleb.programming@gmail.com>
2020-10-25 12:02:29 -0700

    started fireball attack

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,15 @@ extends KinematicBody2D
 const MAX_SPEED = 200
 const ACCELERATION = 10000
 const FRICTION = 10000
+const SHOOT_SPEED = 10;
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 
 var velocity = Vector2.ZERO
 
@@ -13,9 +22,26 @@ onready var animationState = animationTree.get("parameters/playback")
 # called when node and children are ready, chilren _readys run first
 func _ready():
 	print("Hello, world!")
+	animationTree.active = true
 
-# called every physics tick
-func _physics_process(delta):
+# called every tick
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+	
+	
+
+
+
+
+#move state
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -28,6 +54,8 @@ func _physics_process(delta):
 		# makes sure the player will be in the correct idle position when they stop moving
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		# not in attack state because we dont want the player to be able to change direction mid animation
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		# sets to run animation
 		animationState.travel("Run")
 		# delta needs to be applied to anything that shouldnt be frame dependent and should work in real time
@@ -39,5 +67,27 @@ func _physics_process(delta):
 		# slows the player down to zero speed
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	# prevents colission errors in corners
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity)	
+	
+	if Input.is_action_just_pressed("ui_fire"):
+		shoot_fireball()
+		state = ATTACK
 
+func attack_state(delta):
+	animationState.travel("Attack")
+	
+
+func attack_animation_finished():
+	state = MOVE
+	
+func shoot_fireball():
+	var FireballScene = load("res://World/Fireball.tscn")
+	var fireball = FireballScene.instance()
+	var fireball_rotation = self.get_angle_to(get_global_mouse_position() - global_position )  
+	
+	print (fireball_rotation);
+	var world = get_tree().current_scene
+	fireball.global_position = global_position
+	fireball.rotation = fireball_rotation
+	world.add_child(fireball)
+	fireball.apply_impulse(Vector2(), ( get_global_mouse_position() - global_position ) * SHOOT_SPEED)
