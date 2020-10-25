768d71ad8df5498babbdc3a88da5304c73d318ee
Joseph Lund <skleb.programming@gmail.com>
2020-10-25 16:24:06 -0700

    attack animation doesnt stop player movement now

--- Player/Player.gd
+++ Player/Player.gd
@@ -26,22 +26,13 @@ func _ready():
 
 # called every tick
 func _process(delta):
-	match state:
-		MOVE:
-			move_state(delta)
-		ROLL:
-			pass
-		ATTACK:
-			attack_state(delta)
-	
-	
-	
-
-
-
-
-#move state
-func move_state(delta):
+#	match state:
+#		MOVE:
+#			move_state(delta)
+#		ROLL:
+#			pass
+#		ATTACK:
+#			attack_state(delta)
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -56,26 +47,37 @@ func move_state(delta):
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		# not in attack state because we dont want the player to be able to change direction mid animation
 		animationTree.set("parameters/Attack/blend_position", input_vector)
-		# sets to run animation
-		animationState.travel("Run")
+
+		print(state)
+		match state:
+			MOVE:
+				# sets to run animation
+				animationState.travel("Run")
+			ROLL:
+				pass
+			ATTACK:
+				animationState.travel("Attack")
 		# delta needs to be applied to anything that shouldnt be frame dependent and should work in real time
 		# like player movement
 		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationState.travel("Idle")
+		match state:
+			MOVE:
+				# sets to run animation
+				animationState.travel("Idle")
+			ROLL:
+				pass
+			ATTACK:
+				animationState.travel("Attack")
 		# slows the player down to zero speed
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	# prevents colission errors in corners
-	velocity = move_and_slide(velocity)	
-	
 	if Input.is_action_just_pressed("ui_fire"):
-		shoot_fireball()
+		animationState.travel("Attack")
 		state = ATTACK
-
-func attack_state(delta):
-	animationState.travel("Attack")
-	
+		shoot_fireball()
+	velocity = move_and_slide(velocity)
 
 func attack_animation_finished():
 	state = MOVE
@@ -83,11 +85,8 @@ func attack_animation_finished():
 func shoot_fireball():
 	var FireballScene = load("res://World/Fireball.tscn")
 	var fireball = FireballScene.instance()
-	print(self.global_position)
-	print(get_global_mouse_position())
 	var fireball_rotation = self.get_angle_to(get_global_mouse_position())  
 	
-#	print (fireball_rotation);
 	var world = get_tree().current_scene
 	fireball.global_position = global_position
 	fireball.rotation = fireball_rotation
