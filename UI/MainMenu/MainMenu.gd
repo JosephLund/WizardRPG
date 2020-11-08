0a533585a3d1a775c88c44100754ab36fb94911e
Joseph Lund <skleb.programming@gmail.com>
2020-11-07 20:26:34 -0800

    finished login basics

--- UI/MainMenu/MainMenu.gd
+++ UI/MainMenu/MainMenu.gd
@@ -11,14 +11,24 @@ onready var notification = $Notification
 
 onready var http : HTTPRequest = $HTTPRequest
 
+enum {LOGIN, REGISTER, PASSWORD, CONFIRMPASSWORD}
+
+var state = LOGIN
+var inputFocus = null
+var disabled = false
 
 var Firebase = load("res://Firebase.gd").new()
 
 func _ready():
-	pass
+	state = LOGIN
 
-
-
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if !disabled:
+			if inputFocus == PASSWORD:
+				_on_LoginButton_pressed()
+			if inputFocus == CONFIRMPASSWORD:
+				_on_ConfirmRegisterButton_pressed()
 
 func _on_RegisterButton_pressed():
 	# hides login items
@@ -29,6 +39,7 @@ func _on_RegisterButton_pressed():
 	confirmPassword.visible = true
 	confirmRegisterButton.visible = true
 	backButton.visible = true
+	state = REGISTER
 
 
 func _on_BackButton_pressed():
@@ -42,24 +53,66 @@ func _on_BackButton_pressed():
 	backButton.visible = false
 	
 	notification.text = ""
+	state = LOGIN
 
 
-func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
+func _on_HTTPRequest_request_completed(_result: int, response_code: int, _headers: PoolStringArray, body: PoolByteArray) -> void:
+	enableButtons()
 	var response_body := JSON.parse(body.get_string_from_ascii())
 	if response_code != 200:
 		notification.text = response_body.result.error.message.capitalize()
 	else:
-		notification.text = "Registration Successful!"
-		yield(get_tree().create_timer(2.0), "timeout")
-		_on_BackButton_pressed()
+		if state == REGISTER:
+			notification.text = "Registration Successful!"
+			yield(get_tree().create_timer(2.0), "timeout")
+			_on_BackButton_pressed()
+		else:
+			notification.text = "Login Successful!"
 
 
 func _on_ConfirmRegisterButton_pressed():
 	if password.text != confirmPassword.text or email.text.empty() or password.text.empty():
 		notification.text = "Invalid password or username"
 		return
+	disableButtons()
 	Firebase.register(email.text, password.text, http)
 
 
 func _on_LoginButton_pressed():
-	pass # Replace with function body.
+	if email.text.empty() or password.text.empty():
+		notification.text = "Invalid password or username"
+		return
+	disableButtons()
+	Firebase.login(email.text, password.text, http)
+
+
+func _on_Password_focus_entered():
+	inputFocus = PASSWORD
+
+
+func _on_Password_focus_exited():
+	inputFocus = null
+
+
+func _on_ConfirmPassword_focus_entered():
+	inputFocus = CONFIRMPASSWORD
+
+
+func _on_ConfirmPassword_focus_exited():
+	inputFocus = null
+
+
+
+func disableButtons():
+	disabled = true
+	loginButton.disabled = true
+	registerButton.disabled = true
+	confirmRegisterButton.disabled = true
+	backButton.disabled = true
+
+func enableButtons():
+	disabled = false
+	loginButton.disabled = false
+	registerButton.disabled = false
+	confirmRegisterButton.disabled = false
+	backButton.disabled = false
