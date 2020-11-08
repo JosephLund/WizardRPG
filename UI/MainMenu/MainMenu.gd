be45985a2c79d150a5bd570bbb16367a2ffe479a
Joseph Lund <skleb.programming@gmail.com>
2020-11-07 19:55:19 -0800

    registration works

--- UI/MainMenu/MainMenu.gd
+++ UI/MainMenu/MainMenu.gd
@@ -5,8 +5,14 @@ onready var registerButton = $RegisterButton
 onready var confirmRegisterButton = $ConfirmRegisterButton
 onready var backButton = $BackButton
 onready var confirmPassword = $ConfirmPassword
+onready var email = $Email
+onready var password = $Password
+onready var notification = $Notification
+
+onready var http : HTTPRequest = $HTTPRequest
 
 
+var Firebase = load("res://Firebase.gd").new()
 
 func _ready():
 	pass
@@ -34,3 +40,26 @@ func _on_BackButton_pressed():
 	confirmPassword.visible = false
 	confirmRegisterButton.visible = false
 	backButton.visible = false
+	
+	notification.text = ""
+
+
+func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
+	var response_body := JSON.parse(body.get_string_from_ascii())
+	if response_code != 200:
+		notification.text = response_body.result.error.message.capitalize()
+	else:
+		notification.text = "Registration Successful!"
+		yield(get_tree().create_timer(2.0), "timeout")
+		_on_BackButton_pressed()
+
+
+func _on_ConfirmRegisterButton_pressed():
+	if password.text != confirmPassword.text or email.text.empty() or password.text.empty():
+		notification.text = "Invalid password or username"
+		return
+	Firebase.register(email.text, password.text, http)
+
+
+func _on_LoginButton_pressed():
+	pass # Replace with function body.
